2a49df7a11aa5ed3047f9ee2149993dcc2d8ef28
Anwesh43 <anweshthecool0@gmail.com>
2018-02-01 06:05:21 +0530

    adding CircleToPolygon class

--- circletopolygonalview/src/main/java/ui/anwesome/com/circletopolygonalview/CircleToPolygonalView.kt
+++ circletopolygonalview/src/main/java/ui/anwesome/com/circletopolygonalview/CircleToPolygonalView.kt
@@ -19,4 +19,39 @@ class CircleToPolygonalView(ctx:Context,var n:Int = 3):View(ctx) {
         }
         return true
     }
+    data class CircleToPolygon(var x:Float,var y:Float,var r:Float,var n:Int) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            if(n >= 3) {
+                paint.color = Color.BLUE
+                val deg = 360f/n
+                val yr = r*Math.sin((deg/2)*Math.PI/180).toFloat()
+                canvas.save()
+                canvas.translate(x,y)
+                for(i in 0..n-1) {
+                    canvas.save()
+                    canvas.rotate(deg*i)
+                    val path = Path()
+                    for(j in 0..deg.toInt()) {
+                        val px = r*Math.cos(j*Math.PI/180).toFloat()
+                        val py = yr*Math.sin(j*Math.PI/180).toFloat()
+                        if(j == 0) {
+                            path.moveTo(px,py)
+                        }
+                        else {
+                            path.lineTo(px,py)
+                        }
+                    }
+                    canvas.drawPath(path,paint)
+                    canvas.restore()
+                }
+                canvas.restore()
+            }
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
