3fabbe1894d329fc4b8ae5ea262c92c85b613b86
Anwesh43 <anweshthecool0@gmail.com>
2018-02-01 07:40:49 +0530

    creating views in Activity

--- app/src/main/java/ui/anwesome/com/kotlincircletopolygonalview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircletopolygonalview/MainActivity.kt
@@ -2,11 +2,21 @@ package ui.anwesome.com.kotlincircletopolygonalview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circletopolygonalview.CircleToPolygonalView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val square = CircleToPolygonalView.create(this,100,4)
+        val triangle =  CircleToPolygonalView.create(this,100)
+        val pentagon = CircleToPolygonalView.create(this,100,5)
+        val hexagon = CircleToPolygonalView.create(this,100,6)
+        square.x = 0f
+        triangle.x = 300f
+        triangle.y = 300f
+        pentagon.x = 300f
+        pentagon.y = 0f
+        hexagon.x = 600f
     }
 }
